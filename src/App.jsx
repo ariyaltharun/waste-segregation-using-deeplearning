2115e41de6eeb6617b68d8c222908d0cabfd59f1
ariyaltharun <ariyaltharun@gmail.com>
2024-05-15 08:50:48 +0000

    add navbar component and router

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,78 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import About from './Components/About'
+import Footer from './Components/Footer'
 
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Evaluation from './Components/Evaluation'
+import Features from './Components/Features'
+import EvaluationResult from './Components/EvaluationResult'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+    <>
+      <Navbar />
+      <Home />
+      <Features />
+      <Footer />
+    </>
+  },
+  {
+    path: "/About",
+    element:
+    <>
+      <Navbar />
+      <About />
+      <Footer />
+    </>
+  },
+  // {
+  //   path: "/UploadImageFolder",
+  //   element: 
+  //   <>
+  //     <Navbar />
+  //     <UploadImagesFolder />
+  //     <Footer />
+  //   </>
+  // },
+  {
+    path: "/Evaluation",
+    element: 
+    <>
+      <Navbar />
+      <Evaluation />
+      <Footer />
+    </>
+  },
+  {
+    path: "/EvaluationResult",
+    element:
+    <>
+      <Navbar />
+      <EvaluationResult />
+      <Footer />
+    </>
+  }
+])
+
+function App() {
+
+  return <RouterProvider router={router} />
   return (
     <>
-      <div>
+    <NavBar />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <Footer />
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +93,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
